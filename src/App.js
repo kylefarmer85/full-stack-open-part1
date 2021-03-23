c700a69f79cfac7bbd5237576af0760dfc07d09f
kylefarmer85 <kylefarmer85@gmail.com>
2021-03-23 17:19:11 -0500

    anecdotes

--- src/App.js
+++ src/App.js
@@ -158,76 +158,132 @@ import React, { useState } from 'react';
 
 ///////// 1d unicafe
 
-const Button = ({ handleClick, text }) => (
+// const Button = ({ handleClick, text }) => (
+//   <button onClick={handleClick}>{text}</button>
+// );
+
+// const Statistics = ({ good, bad, neutral }) => {
+//   const all = () => {
+//     return good + bad + neutral;
+//   };
+
+//   const average = () => {
+//     let goodScore = good;
+//     let badScore = bad;
+
+//     return (goodScore - badScore) / all();
+//   };
+
+//   const positive = () => {
+//     let pos = good / all();
+//     return <>{pos / 0.1} %</>;
+//   };
+
+//   return (
+//     <>
+//       {all() ? (
+//         <div>
+//           <Statistic text='good' value={good} />
+//           <Statistic text='neutral' value={neutral} />
+//           <Statistic text='bad' value={bad} />
+//           <Statistic text='all' value={all()} />
+//           <Statistic text='average' value={average()} />
+//           <Statistic text='postitive' value={positive()} />
+//         </div>
+//       ) : (
+//         <p>No feedback given</p>
+//       )}
+//     </>
+//   );
+// };
+
+// const Statistic = ({ text, value }) => (
+//   <p>
+//     {text} {value}
+//   </p>
+// );
+
+// const App = () => {
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   const incrementGood = () => {
+//     setGood(good + 1);
+//   };
+//   const incrementBad = () => {
+//     setBad(bad + 1);
+//   };
+//   const incrementNeutral = () => {
+//     setNeutral(neutral + 1);
+//   };
+
+//   return (
+//     <div>
+//       <h1>give feedback</h1>
+//       <Button handleClick={incrementGood} text='good' />
+//       <Button handleClick={incrementNeutral} text='neutral' />
+//       <Button handleClick={incrementBad} text='bad' />
+
+//       <h1>statistics</h1>
+//       <Statistics good={good} bad={bad} neutral={neutral} />
+//     </div>
+//   );
+// };
+
+//////////////////////// 1d anecdotes
+
+const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Statistics = ({ good, bad, neutral }) => {
-  const all = () => {
-    return good + bad + neutral;
-  };
-
-  const average = () => {
-    let goodScore = good;
-    let badScore = bad;
-
-    return (goodScore - badScore) / all();
-  };
-
-  const positive = () => {
-    let pos = good / all();
-    return <>{pos / 0.1} %</>;
-  };
-
-  return (
-    <>
-      {all() ? (
-        <div>
-          <Statistic text='good' value={good} />
-          <Statistic text='neutral' value={neutral} />
-          <Statistic text='bad' value={bad} />
-          <Statistic text='all' value={all()} />
-          <Statistic text='average' value={average()} />
-          <Statistic text='postitive' value={positive()} />
-        </div>
-      ) : (
-        <p>No feedback given</p>
-      )}
-    </>
-  );
-};
-
-const Statistic = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
-);
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ];
 
-  const incrementGood = () => {
-    setGood(good + 1);
+  const [selected, setSelected] = useState(0);
+  const [obj, setObj] = useState({});
+
+  const getRandom = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
   };
-  const incrementBad = () => {
-    setBad(bad + 1);
+
+  const incrementVote = () => {
+    let value = obj[selected] ? obj[selected] + 1 : 1;
+    setObj({
+      ...obj,
+      [selected]: value
+    });
   };
-  const incrementNeutral = () => {
-    setNeutral(neutral + 1);
+
+  const highestVoted = () => {
+    let index = '';
+    let count = 0;
+    for (let key in obj) {
+      if (obj[key] > count) {
+        index = key;
+        count = obj[key];
+      }
+    }
+    return index;
   };
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={incrementGood} text='good' />
-      <Button handleClick={incrementNeutral} text='neutral' />
-      <Button handleClick={incrementBad} text='bad' />
-
-      <h1>statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {obj[selected]} votes</p>
+      <Button handleClick={incrementVote} text='vote' />
+      <Button handleClick={getRandom} text='next anecdote' />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[highestVoted()]}</p>
+      <p>has {obj[highestVoted()]} votes</p>
     </div>
   );
 };
